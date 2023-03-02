d478c9df31e1375fe93b0680366ee5428e8fdb6f
Spark4444 <102835794+Mista44444444@users.noreply.github.com>
2023-03-02 20:29:54 +0100

    Add files via upload

--- fangame/js/script.js
+++ fangame/js/script.js
@@ -19,6 +19,7 @@ let attack_gif = document.querySelector(".attack_png");
 let attack_line = document.querySelector(".line_attack");
 let hp_string = document.querySelector(".txt").innerHTML[0] + document.querySelector(".txt").innerHTML[1];
 let hp_width = document.querySelector(".hpVis");
+let canvas = document.querySelector("#canvas");
 let food_list = ["Pie","I. Noodles","Steak","L. Hero","L. Hero","L. Hero","L. Hero","L. Hero"];
 let health = ["72","72","60","40","40","40","40","40"];
 let text = "* Mettaton NEO blocks the way!";
@@ -41,11 +42,11 @@ let num_second = 0;
 let time = -200;
 let time2 = 0;
 let mettaton_hp = 1000;
-//console.log(alert("Click this so the game starts"));
 
 //Width for hp bar
 hp_width.style.width = "100%"
 
+//Waits for the button to be clicked and here you can change the animation in the start parameter
 start_button.addEventListener('click', () =>{
   html.classList.add("cursor_none");
   frisk.classList.remove("hidden");
@@ -97,6 +98,153 @@ function no_start_animation(){
   // no_start_animation();
 });
 
+//All functions initialization
+//Function that will check for the num and then display check
+function check(num){
+  if(num_second == 0){
+    img[num_second].src = "img/nothing.png"
+    text = "<div class='attack_text'><img class= 'heart_img' src='img/heart.png'> * Mettaton NEO  <div class='hp_show_wrap'><div class='hp_shower fisrt_hp'></div></div></div>";
+    typeWriter2();
+  }
+  if(num_second == 1){
+    img[num_second].src = "img/nothing.png"
+    text = "<img class= 'heart_img' src='img/heart.png'> * Mettaton NEO";
+    typeWriter2();
+
+  }
+  if(num_second == 2){
+    img[num_second].src = "img/nothing.png"
+    position = 0;
+    text =  ` <img class="heart_img" src="img/nothing.png"> * ${food_list[0]}<img class="heart_img i1" src="img/nothing.png"> * ${food_list[1]}
+ <img class="heart_img" src="img/nothing.png"> * ${food_list[2]}<img class="heart_img i2" src="img/nothing.png"> * ${food_list[3]}
+<div class="p1">PAGE 1</div>`;
+    typeWriter2();
+    imgH();
+    img_h[0].src = "img/heart.png"
+  }
+  if(num_second == 3){
+    img[num_second].src = "img/nothing.png"
+    text = "<img class= 'heart_img' src='img/heart.png'> * Spare";
+    typeWriter2();
+  }
+}
+
+//Second checker
+function check2(num){
+  if(num_second == 0){
+    x_disable = false;
+    stage = false;
+    num = false;
+    text = `<div class="attack_wrap"><img src="img/attack.png" class="attack"></div>`
+    typeWriter2();
+    attack_function();
+  }
+  if(num_second == 1){
+    text = `<img class= 'heart_img' src='img/heart.png'> * Check`;
+    typeWriter2 ();
+  }
+  if(num_second == 2){
+    x_disable = false;
+    food = food_list[position];
+    hp_recover = health[position];
+    food_list.splice(position, 1);
+    food_list[8-position] = " ";
+    health.splice(position, 1);
+    health[8-position] = "0";
+
+    if(food == "Pie"){
+      text = `* You ate the ${food}.
+* Your HP was maxed out!`;
+    }
+    else if(food == "Steak"){
+      text = `* You ate the ${food}.
+* You recovered ${hp_recover} HP!`;
+    }
+    else if(food == "I. Noodles"){
+      text = `* You ate the Instant Noodles.
+* Your HP was maxed out!`;
+    }
+    else if(food == "L. Hero"){
+      text = `* You eat the Legerndary Hero.
+* You recovered ${hp_recover} HP!`;
+    }
+    hp_string = document.querySelector(".txt").innerHTML[0] + document.querySelector(".txt").innerHTML[1];
+    HP_recover(hp_recover,hp_string,hp,hp_width);
+    typeWriter();
+    position = false;
+  }
+  if(num_second == 3){
+    x_disable = false;
+    disappear();
+  }
+}
+
+//Third checker
+function check3(num){
+  if(num_second == 0){
+    //continue
+  }
+  if(num_second == 1){
+    x_disable = false;
+    text = `* METTATON NEO - 90 ATK 9 DEF
+* Dr. Alphys's greatest
+  invention.`;
+    typeWriter();
+  }
+  if(num_second == 2){
+    num = false;
+    stage = false;
+    disappear();
+  if(num_second == 3){
+  }
+}
+}
+
+//Fourth checker
+function check4(num){
+  if(num_second == 0){
+  }
+  if(num_second == 1){
+    num = false;
+    stage = false;
+    disappear();
+  }
+  if(num_second == 2){
+  }
+  if(num_second == 3){
+  }
+}
+
+//text_menu disappear
+function disappear(){
+  text = "";
+  typeWriter2();
+  text_placeholder.classList.add("cube")
+  setTimeout(function(){
+    text_placeholder.classList.add("hidden");
+    buttons[num_second].classList.remove('yellow');
+    img[num_second].src = "";
+    canvas.style.display = "";
+  },1000);
+}
+
+//text_menu appear
+function appear(){
+  num = 0;
+  classA();
+  stage = 0;
+  canvas.style.display = "none";
+  text_placeholder.classList.remove("hidden");
+  setTimeout(function(){
+    text_placeholder.classList.remove("cube");
+  },100);
+  
+  setTimeout(function(){
+    text = "* Stage lights are blaring";
+    typeWriter();
+  },1000 + 100);
+}
+
 //img_h initialization
 function imgH(){
   img_h = document.querySelectorAll(".heart_img");  
@@ -130,6 +278,7 @@ function attack_function(){
       setTimeout(function() {
         attack_line.classList.add("hidden");
         elem_mettaton.classList.add("hidden");
+        disappear();
       }, 900);
     }
     time = -50;
@@ -149,9 +298,10 @@ function HP_recover(health,HP,HP_text,HP_width){
   }
 }
 
+//Removes img that is needed to make the inventory work because of the DOM initialization
 img_h[0].remove();
 
-//Movement between buttons function
+//Movement between buttons
 function classA(){
   num_second = num;
   if(num_second > 3){
@@ -202,7 +352,34 @@ function classA(){
   buttons[num].classList.add('yellow');
 }
 
-//A,a and left arrow keys listeners
+//Timeout
+let timeoutID;
+
+//Function that writes the text in the text box letter by letter
+function typeWriter() {
+  text_placeholder.innerHTML = "";
+  clearTimeout(timeoutID);
+  let i = 0;
+  timeoutID = setInterval(() => {
+    if (i >= text.length) {
+      clearInterval(timeoutID);
+      return;
+    }
+    text_placeholder.innerHTML += text[i];
+    i++;
+  }, 40);
+}
+
+//Function that writes the text in the text box instantly
+function typeWriter2() {
+  clearTimeout(timeoutID);
+  text_placeholder.innerHTML = "";
+  text_placeholder.innerHTML = text;
+  return;
+}
+//End of the functions initialization
+
+//A,a and left arrow keys listeners, movement to the right of the buttons
 document.addEventListener('keyup', e => {
   const key = e.key;
   switch (key) {
@@ -220,7 +397,7 @@ document.addEventListener('keyup', e => {
   }
 });
 
-//D,d and right arrow keys listener
+//D,d and right arrow keys listener, movement to the left of the buttons
 document.addEventListener('keyup', e => {
   const key = e.key;
   switch (key) {
@@ -245,6 +422,7 @@ document.addEventListener('keyup', e => {
     case 'Enter':
     case 'z':
     case 'Z':
+      //If the user pressed enter while the attack
       if(attack === 0 && stage == false && attack !== false){
         attack = false;
         time2 = time/1000;
@@ -253,6 +431,9 @@ document.addEventListener('keyup', e => {
           time2 = 1.11 - time2;
         }
         attack2 = (time2/(0.8/100)/2 * (36/100)).toFixed(0);
+        if(attack2 <= 0 ){
+          attack2 = 0;
+        }
         attack_gif.src = "img/attack.gif";
         attack_gif.classList.remove("hidden");
         elem_mettaton.classList.remove("hidden");
@@ -262,6 +443,7 @@ document.addEventListener('keyup', e => {
         setTimeout(function(){
           elem_mettaton.classList.add("hidden");
           attack_gif.classList.add("hidden");
+          disappear();
         },900);
         width_pr = attack_line.getBoundingClientRect().left / window.innerWidth;
         attack_line.style.left = `${width_pr*100}%`;
@@ -272,7 +454,10 @@ document.addEventListener('keyup', e => {
           attack_line.classList.add("hidden");
           attack_line.classList.remove("appear_hide");
         },400);
+
       }
+
+      //Movement between the text menus
       if(num_second > 3){
         num_second = 0;
       }
@@ -285,31 +470,33 @@ document.addEventListener('keyup', e => {
       if(num < 0){
         num = 3;
       }
-      if(stage == 3 && stage !== false && num == 1){
+      if(stage == 3 && stage !== false && num_second == 1){
         stage = 4;
-        check4(num);
+        check4(num_second);
         audio1.play();
        }
-      if(stage == 2 && stage !== false && num == 0 || stage == 2 && stage !== false && num == 1 || stage == 2 && stage !== false && num == 2){
+      if(stage == 2 && stage !== false && num_second == 0 || stage == 2 && stage !== false && num_second == 1 || stage == 2 && stage !== false && num_second == 2){
         stage = 3;
-        check3(num);
+        check3(num_second);
         audio1.play();
        }
        if(stage == 1 && stage !== false){
         stage = 2;
-        check2(num);
+        check2(num_second);
         audio1.play();
         }
       if(stage == 0 && stage !== false){
         stage = 1;
-        check(num);
+        check(num_second);
         audio1.play();
-        num = false;
         x_disable = true;
+        num = false;
         }
+
       break;
     case 'x':
     case 'X':
+      //Go outn of the text menu
       if(x_disable == true){
       position = false;
       stage = 0;
@@ -321,6 +508,7 @@ document.addEventListener('keyup', e => {
       num = num_second;
       x_disable = false;
       }
+
       break;
       default:
       return;
@@ -330,15 +518,18 @@ document.addEventListener('keyup', e => {
 //Movement in the inventory
 document.addEventListener('keyup', e => {
   if(position !== false){
+    //Sets the text1 to this every time
   text1 = ` <img class="heart_img" src="img/nothing.png"> * ${food_list[0]}<img class="heart_img i1" src="img/nothing.png"> * ${food_list[1]}
  <img class="heart_img" src="img/nothing.png"> * ${food_list[2]}<img class="heart_img i2" src="img/nothing.png"> * ${food_list[3]}
 <div class="p1">PAGE 1</div>`;
   text2 = ` <img class="heart_img" src="img/nothing.png"> * ${food_list[4]}<img class="heart_img i3" src="img/nothing.png"> * ${food_list[5]}
  <img class="heart_img" src="img/nothing.png"> * ${food_list[6]}<img class="heart_img i4" src="img/nothing.png"> * ${food_list[7]} 
 <div class="p2">PAGE 2</div>`; 
+
   const key = e.key;
+  //Movement in  the inventory
   switch (key) {
-      case 'ArrowDownUp':
+      case 'ArrowUp':
       case "W":
       case "w":
         img_h[position2].src = "img/nothing.png";
@@ -356,6 +547,7 @@ document.addEventListener('keyup', e => {
         }
         audio1.play();
         console.log(position);
+
       break;
       case 'ArrowDown':
       case "S":
@@ -375,6 +567,7 @@ document.addEventListener('keyup', e => {
         }
         audio1.play();
         console.log(position);
+
       break;
       case 'ArrowLeft':
       case "A":
@@ -400,6 +593,7 @@ document.addEventListener('keyup', e => {
         }
         audio1.play();
         console.log(position);
+
       break;
       case 'ArrowRight':
       case "D":
@@ -431,176 +625,37 @@ document.addEventListener('keyup', e => {
         img_h[position2].src = "img/heart.png";
         audio1.play();
         console.log(position);
+
       break;
     default:
       return;
-}
-if(position == 4){
-  position2 = 0;
-}
-else if(position == 5){
-  position2 = 1;
-}
-else if(position == 6){
-  position2 = 2;
-}
-else if(position == 7){
-  position2 = 3;
-}
-else if(position > -1 && position < 4){
-  position2 = position;
-}
-if(position > -1 && position < 4){
-  text = text1;
-}
-if(position > 3 && position < 8){
-  text = text2;
-}
+  }
+
+  //Sets the position for the img because theyre different
+  if(position == 4){
+    position2 = 0;
+  }
+  else if(position == 5){
+    position2 = 1;
+  }
+  else if(position == 6){
+    position2 = 2;
+  }
+  else if(position == 7){
+    position2 = 3;
+  }
+  else if(position > -1 && position < 4){
+    position2 = position;
+  }
+  if(position > -1 && position < 4){
+    text = text1;
+  }
+  if(position > 3 && position < 8){
+    text = text2;
+  }
 typeWriter2();
 imgH();
 img_h[position2].src = "img/heart.png";
 
 }
 });
-
-//Timeout
-let timeoutID;
-
-//Function that writes the text in the text box letter by letter
-function typeWriter() {
-  text_placeholder.innerHTML = "";
-  clearTimeout(timeoutID);
-  let i = 0;
-  timeoutID = setInterval(() => {
-    if (i >= text.length) {
-      clearInterval(timeoutID);
-      return;
-    }
-    text_placeholder.innerHTML += text[i];
-    i++;
-  }, 40);
-}
-
-//Function that writes the text in the text box instantly
-function typeWriter2() {
-  clearTimeout(timeoutID);
-  text_placeholder.innerHTML = "";
-  text_placeholder.innerHTML = text;
-  return;
-}
-
-//Function that will check for the num and then display check
-function check(num){
-  if(num_second == 0){
-    img[num_second].src = "img/nothing.png"
-    text = "<div class='attack_text'><img class= 'heart_img' src='img/heart.png'> * Mettaton NEO  <div class='hp_show_wrap'><div class='hp_shower fisrt_hp'></div></div></div>";
-    typeWriter2();
-  }
-  if(num_second == 1){
-    img[num_second].src = "img/nothing.png"
-    text = "<img class= 'heart_img' src='img/heart.png'> * Mettaton NEO";
-    typeWriter2();
-
-  }
-  if(num_second == 2){
-    img[num_second].src = "img/nothing.png"
-    position = 0;
-    text =  ` <img class="heart_img" src="img/nothing.png"> * ${food_list[0]}<img class="heart_img i1" src="img/nothing.png"> * ${food_list[1]}
- <img class="heart_img" src="img/nothing.png"> * ${food_list[2]}<img class="heart_img i2" src="img/nothing.png"> * ${food_list[3]}
-<div class="p1">PAGE 1</div>`;
-    typeWriter2();
-    imgH();
-    img_h[0].src = "img/heart.png"
-  }
-  if(num_second == 3){
-    img[num_second].src = "img/nothing.png"
-    text = "<img class= 'heart_img' src='img/heart.png'> * Spare";
-    typeWriter2();
-  }
-}
-
-//Second checker
-function check2(num){
-  if(num_second == 0){
-    x_disable = false;
-    stage = false;
-    num = false;
-    text = `<div class="attack_wrap"><img src="img/attack.png" class="attack"></div>`
-    typeWriter2();
-    attack_function()
-  }
-  if(num_second == 1){
-    text = `<img class= 'heart_img' src='img/heart.png'> * Check`;
-    typeWriter2 ();
-  }
-  if(num_second == 2){
-    x_disable = false;
-    stage = false;
-    num = false;
-    food = food_list[position];
-    hp_recover = health[position];
-    food_list.splice(position, 1);
-    health.splice(position, 1);
-
-    if(food == "Pie"){
-      text = `* You ate the ${food}.
-* Your HP was maxed out!`;
-    }
-    else if(food == "Steak"){
-      text = `* You ate the ${food}.
-* You recovered ${hp_recover} HP!`;
-    }
-    else if(food == "I. Noodles"){
-      text = `* You ate the Instant Noodles.
-* Your HP was maxed out!`;
-    }
-    else if(food == "L. Hero"){
-      text = `* You eat the Legerndary Hero.
-* You recovered ${hp_recover} HP!`;
-    }
-    hp_string = document.querySelector(".txt").innerHTML[0] + document.querySelector(".txt").innerHTML[1];
-    HP_recover(hp_recover,hp_string,hp,hp_width);
-    typeWriter2();
-    position = false;
-  }
-  if(num_second == 3){
-    x_disable = false;
-    stage = false;
-    num = false;
-    //continue
-  }
-}
-
-//Third checker
-function check3(num){
-  if(num_second == 0){
-    //continue
-  }
-  if(num_second == 1){
-    x_disable = false;
-    stage = false;
-    num = false;
-    text = `* METTATON NEO - 90 ATK 9 DEF
-* Dr. Alphys's greatest
-  invention.`;
-    typeWriter2 ();
-  }
-  if(num_second == 2){
-    //continue
-  }
-  if(num_second == 3){
-  }
-}
-
-//Fourth checker
-function check4(num){
-  if(num_second == 0){
-  }
-  if(num_second == 1){
-    // continue here
-  }
-  if(num_second == 2){
-  }
-  if(num_second == 3){
-  }
-}
