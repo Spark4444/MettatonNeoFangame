14739146e680e25bee0ad820aed5c42f5e074731
Spark4444 <102835794+Mista44444444@users.noreply.github.com>
2024-01-18 20:20:13 +0100

    Changes

--- fangame/js/screenBindings.js
+++ fangame/js/screenBindings.js
@@ -1,6 +1,6 @@
-// if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-//     document.body.innerHTML = "<div class='not-working'><h1>You can't play this game on a phone or a tablet because phones and tablets don't have keyboards.</h1></div>";
-// }
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    document.body.innerHTML = "<div class='not-working'><h1>You can't play this game on a phone or a tablet because phones and tablets don't have keyboards.</h1></div>";
+}
 
 const screenWidth = window.screen.width;
 const screenHeight = window.screen.height;
